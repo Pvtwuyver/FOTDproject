2d93621b1ae4643b2067607eec63a1eff70e9702
sophiehutten <sophie.hutten@quicknet.nl>
2015-10-08 16:00:56 +0200

    Delete create_total_per_postcode.sh

--- create_total_per_postcode.sh
+++
@@ -1,7 +0,0 @@
-cat Numbersonly.txt | cut -f 2 | sort | uniq -c > totals_per_code.txt
-# GEEFT TOTAAL VERKOOP PER POSTCODE
-
-cat totals_per_code.txt | sed 's/[ ]/	/g'  > tabseperatedtotals.txt
-# Tab tussen totals en postcode (Herkent R helaas niet)
-
-
